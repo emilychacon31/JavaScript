158c4e0ca241c476d71324f07e3071212fc61a61
emilychacon31 <emilychacon31@outlook.com>
2022-02-03 16:38:53 -0600

    Agregando archivos tarea JavaScript

---
+++ app.js
@@ -0,0 +1,13 @@
+var nombre= "Emily";
+var mascota;
+const cedula="1-2345-6789";
+let tieneCarro= false;
+var familia= ["Ashley","Nidia","Sonia"];
+
+mascota={
+    nombre: "Molly",
+    edad: 4,
+    raza: "Doberman"
+};
+console.log(nombre, mascota, cedula, tieneCarro, familia);
+
